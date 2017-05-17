bfbae65c0378afc34cbbdd596d1f7d6aa456893f
stevez <stevezeitoun1@gmail.com>
2017-05-17 15:33:26 +0200

    structure MVC

---
+++ views/header.php
@@ -0,0 +1,40 @@
+<div class="navbar-fixed">
+  <nav class="opacity">
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo" style="padding-left:5px; padding-top:5px;"><img class="logo" src="views/img/logo.png" alt=""></a>
+      <ul class="right hide-on-med-and-down">
+          <li><a href="#modal1">Agissons ensemble</a></li>
+            <div id="modal1" class="modal">
+              <div class="modal-content">
+                <h4>Modal Header</h4>
+                <p>A bunch of text</p>
+              </div>
+              <div class="modal-footer">
+                <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+              </div>
+</div>
+        <li><a href="badges.html">Connexion</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent">
+        <li class="tab"><a href="#test1" style="color:#2e4e74; font-weight:bold;">A Propos</a></li>
+        <li class="tab"><a href="#test2" style="color:#2e4e74; font-weight:bold;" >Ateliers</a></li>
+        <li class="tab"><a href="#test3" style="color:#2e4e74; font-weight:bold;">Nouveautés</a></li>
+        <li class="tab"><a href="#test4" style="color:#2e4e74; font-weight:bold;">Notre Équipe</a></li>
+        <div class="indicator" style="background-color:#ffc0ed;"></div>
+      </ul>
+    </div>
+  </nav>
+  <div id="test1" class="col s12"></div>
+  <div id="test2" class="col s12"></div>
+  <div id="test3" class="col s12"></div>
+  <div id="test4" class="col s12"></div>
+</div>
+</nav>
+</div>
