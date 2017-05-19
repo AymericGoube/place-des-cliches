5dfc17c8c996aafa4af41230f280e10cafc6fecc
abdoulaye <abdoulayedabo@laposte.net>
2017-05-19 10:58:53 +0200

    début de la structure

--- views/view.php
+++ views/view.php
@@ -1,6 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>  </title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
 
-require "header.php";
-require "footer.php";
-
- ?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+	<script src="js/dropdown.js"></script>
+	<script src="js/modal.js"></script>
+	<script src="js/scrollspy.js"></script>
+</body>
+</html>
