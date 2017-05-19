2606db3e6d6c9e7d8ebd44a2ba2975e334b530e3
aymericgoube <aymericgoube@hotmail.fr>
2017-05-19 17:00:25 +0200

    ajout du footer

--- views/view.php
+++ views/view.php
@@ -2,12 +2,14 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>  </title>
+	<title>Place des cliches</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
-	<link rel="stylesheet" type="text/css" href="css/index.css">
-	<link rel="stylesheet" type="text/css" href="css/header.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link rel="stylesheet" type="text/css" href="css/footer.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
+<link rel="stylesheet" type="text/css" href="views/css/index.css">
+	<link rel="stylesheet" type="text/css" href="views/css/header.css">
+	<link rel="stylesheet" type="text/css" href="views/css/main.css">
+	<link rel="stylesheet" type="text/css" href="views/css/footer.css">
 </head>
 <body>
 
