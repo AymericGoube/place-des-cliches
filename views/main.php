c015b575dc9f054c3b7abb9ecdd634a0ae0161fa
stevez <stevezeitoun1@gmail.com>
2017-05-19 16:27:12 +0200

    Slider

--- views/main.php
+++ views/main.php
@@ -1,22 +1,24 @@
+<main>
+    <div class="carousel carousel-slider center" data-indicators="true">
+        <a class="carousel-item" href="#one!"><img src="views/img/ami.png"></a>
+        <a class="carousel-item" href="#two!"><img src="views/img/ami.png"></a>
+        <a class="carousel-item" href="#three!"><img src="views/img/ami.png"></a>
+        <a class="carousel-item" href="#four!"><img src="views/img/ami.png"></a>
+    </div>
 
-  <div class="carousel carousel-slider center" data-indicators="true">
-    <div class="carousel-fixed-item center">
-      <a class="btn waves-effect white grey-text darken-text-2">button</a>
+    <div class="container history">
+        <h1 class="titlehistory">L'ENGAGEMENT À PORTÉE DE CLICS !</h1>
+        <div class="bilan">
+            <p>Il y a plein de <span class="bilanstyle1">stéréotypes de genre</span> qui circulent au quotidien (films, les séries, les
+               publicités, les réseaux sociaux...). Le problème, c’est qu’on les <span class="bilanstyle1">intériorise</span>sans les
+               mettre en question. Or les stéréotypes ont pour conséquence<span class="bilanstyle1"> les préjugés, la discrimination et
+               les inégalités.</span>
+            </p>
+            <p><span class="bilanstyle2">Place des Clichés</span> consiste à faire des <span class="bilanstyle1"> ateliers-débats </span>auprès de jeunes (12-25 ans)
+               pour parler de ces stéréotypes et amener les participants à les remettre en question.
+            </p>
+        </div>
+        <h2 class="titlehistory">Merci pour votre engagement !</h2>
     </div>
-    <div class="carousel-item red white-text" href="#one!">
-      <h2>First Panel</h2>
-      <p class="white-text">This is your first panel</p>
-    </div>
-    <div class="carousel-item amber white-text" href="#two!">
-      <h2>Second Panel</h2>
-      <p class="white-text">This is your second panel</p>
-    </div>
-    <div class="carousel-item green white-text" href="#three!">
-      <h2>Third Panel</h2>
-      <p class="white-text">This is your third panel</p>
-    </div>
-    <div class="carousel-item blue white-text" href="#four!">
-      <h2>Fourth Panel</h2>
-      <p class="white-text">This is your fourth panel</p>
-    </div>
-  </div>
+    
+</main>
--- views/view.php
+++ views/view.php
@@ -4,22 +4,23 @@
 	<meta charset="utf-8">
 	<title>  </title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
-	<link rel="stylesheet" type="text/css" href="css/index.css">
-	<link rel="stylesheet" type="text/css" href="css/header.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<!-- <link rel="stylesheet" type="text/css" href="css/index.css"> -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/header.css"> -->
+	<link rel="stylesheet" type="text/css" href="views/css/main.css">
+	<!-- <link rel="stylesheet" type="text/css" href="css/footer.css"> -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet"> 
 </head>
 <body>
 
 
 
-	<?php require "main.php" ?>
+	<?php require "main.php"; ?>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script src="js/dropdown.js"></script>
-	<script src="js/modal.js"></script>
-	<script src="js/scrollspy.js"></script>
-	<script type="text/javascript" src="js/carrousel.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+	<!-- <script src="js/dropdown.js"></script> -->
+	<!-- <script src="js/modal.js"></script> -->
+	<!-- <script src="js/scrollspy.js"></script> -->
+	<script type="text/javascript" src="views/js/carrousel.js"></script>
 </body>
 </html>
