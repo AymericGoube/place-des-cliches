1d3b1b4707e701a8a40fa6da4bac060d9f3eb0d9
abdoulaye <abdoulayedabo@laposte.net>
2017-05-19 16:49:40 +0200

    travail sur le header

---
+++ views/header.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+
+<nav>
+  <div class="nav-wrapper nav1">
+    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+    <ul id="nav-mobile" class="left hide-on-med-and-down">
+      <li> <a href=""> agissons ensemble </a> </li>
+      <li> <a href=""> presse </a> </li>
+    </ul>
+
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li> <a href="" class="dropdown-button btn" data-activates="login"> connexion </a> </li>
+    </ul>
+      <ul id="login" class="dropdown-content">
+          <li> <a href=""> Utilisateur </a> </li>
+      </ul>
+
+    <ul class="side-nav" id="mobile-demo">
+      <li> <a href=""> agissons ensemble </a> </li>
+      <li> <a href=""> presse </a> </li>
+      <li> <a href="" class="dropdown-button btn" data-activates="login"> connexion </a> </li>
+        <ul id="login" class="dropdown-content">
+          <li> <a href=""> Utilisateur </a> </li>
+        </ul>
+      <li class="tab"> <a href=""> accueil </a> </li>
+      <li class="tab"> <a href=""> l'association </a> </li>
+      <li class="tab"> <a href=""> notre équipe </a> </li>
+      <li class="tab"> <a href=""> actualités </a> </li>
+    </ul>
+
+  </div>
+</nav>
+
+<div class="logo"></div>
+
+<nav>
+  <div class="nav-wrapper nav2">
+    <ul id="nav-mobile" class="hide-on-med-and-down navmid">
+      <li> <a href=""> accueil </a> </li>
+      <li> <a href=""> l'association </a> </li>
+      <li> <a href=""> notre équipe </a> </li>
+      <li> <a href=""> actualités </a> </li>
+    </ul>
+  </div>
+</nav>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+<script src="js/dropdown.js"></script>
+<script src="js/side.js"></script>
