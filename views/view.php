95dac4a34f76291f58f30e29e69a1e246dc4acd8
aymericgoube <aymericgoube@hotmail.fr>
2017-05-22 14:57:39 +0200

    ajout du js et cadre photo

--- views/view.php
+++ views/view.php
@@ -5,14 +5,14 @@
   <title>Place des clichés</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="views/css/index.css">
 	<link rel="stylesheet" type="text/css" href="views/css/header.css">
 	<link rel="stylesheet" type="text/css" href="views/css/main.css">
 	<link rel="stylesheet" type="text/css" href="views/css/footer.css">
 </head>
 <body>
-  
+
   <?php require 'header.php'; ?>
 	<?php require "main.php"; ?>
   <?php require 'footer.php';
@@ -21,7 +21,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 	<script src="js/dropdown.js"></script>
 	<script src="js/modal.js"></script>
-	<script src="js/scrollspy.js"></script>
+	<script src="views/js/scrollspy.js"></script>
 	<script src="views/js/carrousel.js"></script>
 </body>
 </html>
