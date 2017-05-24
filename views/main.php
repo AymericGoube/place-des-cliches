f612a276a3edaf6003734989271f586a7ec91a0d
aymericgoube <aymericgoube@hotmail.fr>
2017-05-24 14:11:38 +0200

    carte équipe en rond

--- views/main.php
+++ views/main.php
@@ -1,86 +1,86 @@
 <main>
-    <div class="carousel carousel-slider center" data-indicators="true">
-        <a class="carousel-item" href="#one!"><img src="views/img/ami.png"></a>
-        <a class="carousel-item" href="#two!"><img src="views/img/ami.png"></a>
-        <a class="carousel-item" href="#three!"><img src="views/img/ami.png"></a>
-        <a class="carousel-item" href="#four!"><img src="views/img/ami.png"></a>
-    </div>
+  <a href=".nav1" class="js-scrollTo btnAbsolute"><i class="fa fa-arrow-circle-o-up fa-3x" aria-hidden="true"></i>
+  </a>
+  <div class="carousel carousel-slider center" data-indicators="true">
+    <a class="carousel-item" href="#one!"><img src="views/img/ami.png"></a>
+    <a class="carousel-item" href="#two!"><img src="views/img/ami.png"></a>
+    <a class="carousel-item" href="#three!"><img src="views/img/ami.png"></a>
+    <a class="carousel-item" href="#four!"><img src="views/img/ami.png"></a>
+  </div>
 
-    <div class="container history">
-        <h1 class="titlehistory">L'ENGAGEMENT À PORTÉE DE CLICS !</h1>
-        <div class="bilan">
-            <p>Il y a plein de <span class="bilanstyle1">stéréotypes de genre</span> qui circulent au quotidien (films, les séries, les
-                publicités, les réseaux sociaux...). Le problème, c’est qu’on les <span class="bilanstyle1">intériorise</span>sans les
-                mettre en question. Or les stéréotypes ont pour conséquence<span class="bilanstyle1"> les préjugés, la discrimination et
-                    les inégalités.</span>
-                </p>
-                <p><span class="bilanstyle2">Place des Clichés</span> consiste à faire des <span class="bilanstyle1"> ateliers-débats </span>auprès de jeunes (12-25 ans)
-                    pour parler de ces stéréotypes et amener les participants à les remettre en question.
-                </p>
-            </div>
-            <h2 class="titlehistory">Merci pour votre engagement !</h2>
+  <div class="container history">
+    <h1 class="titlehistory">L'ENGAGEMENT À PORTÉE DE CLICS !</h1>
+    <div class="bilan">
+      <p>Il y a plein de <span class="bilanstyle1">stéréotypes de genre</span> qui circulent au quotidien (films, les séries, les
+        publicités, les réseaux sociaux...). Le problème, c’est qu’on les <span class="bilanstyle1">intériorise</span>sans les
+        mettre en question. Or les stéréotypes ont pour conséquence<span class="bilanstyle1"> les préjugés, la discrimination et
+          les inégalités.</span>
+        </p>
+        <p><span class="bilanstyle2">Place des Clichés</span> consiste à faire des <span class="bilanstyle1"> ateliers-débats </span>auprès de jeunes (12-25 ans)
+          pour parler de ces stéréotypes et amener les participants à les remettre en question.
+        </p>
+      </div>
+      <h2 class="titlehistory">Merci pour votre engagement !</h2>
+    </div>
+    <div class="partieEquipe">
+      <p class="titlehistory">Voici l'équipe !</p>
+      <div class="row rowEquipe">
+        <div class="col s2 templateCarte">
+          <div class="col s12 carte" id="photo1">
+          </div>
+          <div class="texte">
+            <h6>lorem Ipsum</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+          </div>
         </div>
-        <div class="partieEquipe">
-            <p class="titlehistory">Voici l'équipe !</p>
-        <div class="row">
-            <div class="col s3 templateCarte">
-                <div class="col s12 carte" id="photo1">
-                </div>
-                <div class="texte">
-                    <h6>lorem Ipsum</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                </div>
-            </div>
-            <div class="col s3 templateCarte">
-                <div class="col s12 carte" id="photo2">
-                </div>
-                <div class="texte">
-                    <h6>lorem Ipsum</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                </div>
-            </div>
-            <div class="col s3 templateCarte">
-                <div class="col s12 carte" id="photo3">
-                </div>
-                <div class="texte">
-                    <h6>lorem Ipsum</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                </div>
-            </div>
-            <div class="col s3 templateCarte">
-                <div class="col s12 carte" id="photo4">
-                </div>
-                <div class="texte">
-                    <h6>lorem Ipsum</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                </div>
-            </div>
-            </div>
-<div class="row">
-            <div class="col s3 offset-s1 templateCarte">
-                <div class="col s12 carte" id="photo5">
-                </div>
-                <div class="texte">
-                    <h6>lorem Ipsum</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                </div>
-            </div>
-            <div class="col s3 templateCarte">
-                <div class="col s12 carte" id="photo6">
-                </div>
-                <div class="texte">
-                    <h6>lorem Ipsum</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                </div>
-            </div>
-            <div class="col s3 templateCarte">
-                <div class="col s12 carte" id="photo7">
-                </div>
-                <div class="texte">
-                    <h6>lorem Ipsum</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                </div>
-            </div>
+        <div class="col s2 templateCarte">
+          <div class="col s12 carte" id="photo2">
+          </div>
+          <div class="texte">
+            <h6>lorem Ipsum</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+          </div>
         </div>
-            </div>
-    </main>
+        <div class="col s2 templateCarte">
+          <div class="col s12 carte" id="photo3">
+          </div>
+          <div class="texte">
+            <h6>lorem Ipsum</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+          </div>
+        </div>
+        <div class="col s2 templateCarte">
+          <div class="col s12 carte" id="photo4">
+          </div>
+          <div class="texte">
+            <h6>lorem Ipsum</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+          </div>
+        </div>
+        <div class="col s2 templateCarte">
+          <div class="col s12 carte" id="photo5">
+          </div>
+          <div class="texte">
+            <h6>lorem Ipsum</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+          </div>
+        </div>
+        <div class="col s2 templateCarte">
+          <div class="col s12 carte" id="photo6">
+          </div>
+          <div class="texte">
+            <h6>lorem Ipsum</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+          </div>
+        </div>
+        <div class="col s2 templateCarte">
+          <div class="col s12 carte" id="photo7">
+          </div>
+          <div class="texte">
+            <h6>lorem Ipsum</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
