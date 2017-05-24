2a197819469229ec8f320ae31655f6017b97c86a
abdoulaye <abdoulayedabo@laposte.net>
2017-05-24 16:53:32 +0200

    intégration des photos

--- views/view.php
+++ views/view.php
@@ -24,12 +24,12 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 
+	<script src="views/js/side.js"></script>
 	<script src="views/js/dropdown.js"></script>
 	<script src="views/js/modal.js"></script>
 	<script src="views/js/scrollspy.js"></script>
 	<script src="views/js/carrousel.js"></script>
 	<script type="views/js/accordion.js"></script>
-	<script src="views/js/side.js"></script>
 
 </body>
 </html>
